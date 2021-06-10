d71e523dbfb856ce87fdc65deabf8b3af3994aa9
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-06-10 15:05:32 -0700

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,6 +1,5 @@
-userInput = input("Enter a string to check its word count: ")
-
 def findWordCount(userInput):
+    userInput = input("Enter a string to check its word count: ")
     wordCount = len(re.split(';|.|,|\*|\n|\t', userInput))
     print ("Your sentence has " +  str(wordCount) + " words.")
     return(wordCount)
