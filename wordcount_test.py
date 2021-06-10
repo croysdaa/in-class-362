d647ad3d78c62bd322abc139dd88a2075165f826
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-06-10 14:56:19 -0700

    Update and rename wordcount.py to wordcount_test.py

--- wordcount_test.py
+++ wordcount_test.py
@@ -1,13 +1,7 @@
 import re
 import pytest
 import unittest
-
-userInput = input("Enter a string to check its word count: ")
-
-def findWordCount(userInput):
-    wordCount = len(re.split(';|.|,|\*|\n|\t', userInput))
-    print ("Your sentence has " +  str(wordCount) + " words.")
-    return(wordCount)
+import wordcount
 
 def test_enter():
     assert findWordCount('hi\nthere') == 2
