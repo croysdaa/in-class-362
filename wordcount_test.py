5fecbc130a779b22b68a28ec5025ac5825c19892
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-06-10 15:04:46 -0700

    Update wordcount_test.py

--- wordcount_test.py
+++ wordcount_test.py
@@ -3,20 +3,39 @@ import pytest
 import unittest
 import wordcount
 
-def test_enter():
-    assert findWordCount('hi\nthere') == 2
-    
-def test_tab():
-    assert findWordCount('hi\tthere') == 2
-    
-def test_astrix():
-    assert findWordCount('hi\*there') == 2
-    
-def test_comma():
-    assert findWordCount('hi,there') == 2
-    
-def test_semi():
-    assert findWordCount('hi;there') == 2
-    
-def test_period():
-    assert findWordCount('hi.there') == 2
+class TestWordCount:
+    def test_enter(self):
+        assert 2 == wordcount.findWordCount('hi\nthere')
+
+    def test_tab(self):
+        assert 2 == wordcount.findWordCount('hi\tthere')
+
+    def test_astrix(self):
+        assert 2 == wordcount.findWordCount('hi\*there')
+
+    def test_comma(self):
+        assert 2 == wordcount.findWordCount('hi,there')
+
+    def test_semi(self):
+        assert 2 == wordcount.findWordCount('hi;there')
+
+    def test_period(self):
+        assert 2 == wordcount.findWordCount('hi.there')
+class TestWordCountUnit:
+    def test_enter(self):
+        self.assertEqual(2, wordcount.findWordCount('hi\nthere'))
+        
+    def test_tab(self):
+        self.assertEqual(2, wordcount.findWordCount('hi\tthere'))
+
+    def test_astrix(self):
+        self.assertEqual(2, wordcount.findWordCount('hi\*there'))
+                         
+    def test_comma(self):
+        self.assertEqual(2, wordcount.findWordCount('hi,there'))
+
+    def test_semi(self):
+        self.assertEqual(2, wordcount.findWordCount('hi;there'))
+
+    def test_period(self):
+        self.assertEqual(2, wordcount.findWordCount('hi.there'))
