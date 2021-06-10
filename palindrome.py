449ca69c44e0abe00dbd80c94619e9fcee01a037
croysdaa <80421676+croysdaa@users.noreply.github.com>
2021-06-10 15:08:42 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,3 @@
-#import difflib
-
 userInput = input("Enter a string to check if it's a palindrome: ")
 i = len(userInput)
 backwardsString = ""
@@ -11,5 +9,3 @@ if (str(userInput.strip('\n')) != str(backwardsString.strip())):
     print("not a palindrome")
 elif (str(userInput.strip('\n')) == str(backwardsString.strip())):
     print("a palindrome.")
-#diff = list(difflib.ndiff([userInput], [backwardsString]))
-#print ("\n".join(list(diff)))
